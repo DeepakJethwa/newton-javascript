a12ac4de4f051fc39b39da5f88e332107df976ae
deepak jethwa <deepakjethwa95@gmail.com>
2022-09-29 19:29:36 +0530

    intial code

---
+++ notes.md
@@ -0,0 +1,7 @@
+127.0.0.1 = http://localhost
+
+local host = 127.0.0.1
+
+http://127.0.0.1:5500/index.html
+
+protocol://ip-address:port/index.html
---
+++ script.js
@@ -0,0 +1,18 @@
+// var person = {
+//     name: "deepak",
+//     age: 28,
+//     education:{
+//         highschool: "pass"
+//     }
+
+// }
+
+
+// var person = new Object();
+// person.name = "deepak";
+// person.age = 28;
+// person.education ="b.com"
+
+function userclicked(){
+    alert("hello deepak");
+}
